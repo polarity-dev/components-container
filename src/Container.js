629f93ca3158468b0912cf11df13d3cda940d8f8
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2019-10-01 16:01:53 +0200

    refactoring in register function: the options object is assigned to componentConfig by references; this you can use getters inside options and preserve them into components

--- src/Container.js
+++ src/Container.js
@@ -22,12 +22,29 @@ class Container extends EventEmitter {
     this.wrappers = new Map()
   }
 
-  register(componentConfig, options) {
-    componentConfig.options = Object.assign({
+  register(componentConfig, options = {}) {
+    const defaults = {
       debug: this.debug,
       noColors: this.noColors,
       checkStatusInterval: 5 /*minutes*/ * 60000
-    }, componentConfig.options, options)
+    }
+
+    Object.entries(defaults).forEach(([key, value]) => {
+      if (typeof componentConfig[key] === "undefined") {
+        componentConfig[key] = value
+      }
+    })
+
+    if (typeof componentConfig.options === "object") {
+      Object.entries(componentConfig.options).forEach(([key, value]) => {
+        if (typeof options[key] === "undefined") {
+          options[key] = value
+        }
+      })
+    }
+
+    componentConfig.options = options
+
     const wrapper = new ComponentWrapper(this, componentConfig)
     this.wrappers.set(wrapper.name, wrapper)
     return this
